d75b9e73c081d94a3d83b1610993d4008aa710d7
warnuki <stouvenot.faustin@gmail.com>
2021-12-22 02:13:59 +0100

    JS try

--- script/script.js
+++ script/script.js
@@ -107,3 +107,57 @@ $('span').click(function () {
   // Mettre à jour l'index
 index = indexN;
 });
+
+// Variable diapo
+
+let compteur = 0;
+let timer, elements, slides, slideWidth
+
+window.onload = () => {
+  const diapo = document.querySelector(".diapo");
+  elements = document.querySelector(".elements");
+  slides = Array.from(elements.children);
+  slideWidth = diapo.getBoundingClientRect().width;
+
+  let next = document.querySelector("#nav-droite");
+  let prev = document.querySelector("#nav-gauche");
+
+
+  next.addEventListener("click", slideNext);
+  prev.addEventListener("click", slidePrev);
+
+  diapo.addEventListener("mouseover", stopTimer);
+  diapo.addEventListener("mouseout", startTimer);
+
+  window.addEventListener("resize", () => {
+    slideWidth = diapo.getBoundingClientRect().width;
+    slideNext();
+  })
+}
+
+function slideNext(){
+  compteur ++;
+  if(compteur == slides.length){
+    compteur = 0;
+  }
+  let decal = -slideWidth * compteur;
+  elements.style.transform = `translateX(${decal}px)`;
+}
+
+function slidePrev(){
+  compteur --;
+  if(compteur < 0){
+    compteur = slides.length -1;
+  }
+  let decal = -slideWidth * compteur;
+  elements.style.transform = `translateX(${decal}px)`;
+}
+function stopTimer(){
+  clearInterval(timer);
+}
+
+function startTimer(){
+  timer = setinterval(slideNext, 4000);
+}
+
+}

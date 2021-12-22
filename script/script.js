bebf645e65a60c433297ca175f4ca1a3186e9634
warnuki <stouvenot.faustin@gmail.com>
2021-12-22 17:10:45 +0100

    final

--- script/script.js
+++ script/script.js
@@ -1,4 +1,59 @@
+let etatLecteur;
 
+function lecteurPret(event) {
+  // event.target = lecteur
+  event.target.setVolume(50);
+}
+
+function changementLecteur(event) {
+  // event.data = état du lecteur
+  etatLecteur = event.data;
+}
+
+let lecteur;
+
+function onYouTubeIframeAPIReady() {
+  lecteur = new YT.Player("video", {
+    height: "390",
+    width: "640",
+    videoId: "M7lc1UVf-VE",
+    playerVars: {
+      color: "white",
+      enablejsapi: 1,
+      modestbranding: 1,
+      rel: 0
+    },
+    events: {
+      onReady: lecteurPret,
+      onStateChange: changementLecteur
+    }
+  });
+}
+
+// Hauteur de la vidéo
+const hauteurVideo = $("#video").height();
+// Position Y de la vidéo
+const posYVideo = $("#video").offset().top;
+// Valeur declenchant la modification de l'affichage (choix "esthétique")
+const seuil = posYVideo + 0.75 * hauteurVideo;
+
+// Gestion du défilement
+$(window).scroll(function () {
+  // Récupération de la valeur du défilement vertical
+  const scroll = $(window).scrollTop();
+
+  // Classe permettant l'exécution du CSS
+  $("#video").toggleClass(
+    "scroll",
+    etatLecteur === YT.PlayerState.PLAYING && scroll > seuil
+  );
+});
+
+
+
+
+
+      
 let carte = L.map('carte', {
   center: [47.2608333, 2.4188888888888886], // Centre de la France
   zoom: 10,
